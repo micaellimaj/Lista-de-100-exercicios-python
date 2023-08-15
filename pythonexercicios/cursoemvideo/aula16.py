10d27292a2e2b11b51583726794c2f691c36dfe7
Micael josé de Lima <micaeljose144@gmail.com>
2023-08-15 16:14:17 -0300

    aula16

---
+++ pythonexercicios/cursoemvideo/aula16.py
@@ -0,0 +1,10 @@
+Lanche = ('Hamburguer','Suco','Pizza','Pudim')
+#for comida in Lanche:
+    #print(f'Eu vou comer {comida}')
+for cont in range(0, len(Lanche)):
+    print(f'Eu vou comer {Lanche[cont]} na posição {cont}')
+print('Comi pra caramba!')
+#print(len(Lanche))
+# for pos, comida in enumerate(lanche):
+    #print(f'Eu vou comer {comida} na posição {pos}')
+print(sorted(Lanche))
