a3a7defc7e3c6b7ad9b87cbfbc259394a7ffe6da
Micael josé de Lima <micaeljose144@gmail.com>
2023-10-24 14:18:39 -0300

    atividade 107

---
+++ pythonexercicios/ex107/teste.py
@@ -0,0 +1,7 @@
+from moeda import metade, dobro, aumentar, diminuir
+
+p = float(input('Digite o preço: R$ '))
+print(f'A metade de R${p} é R${metade(p)}')
+print(f'O dobro de R${p} é R${dobro(p)}')
+print(f'Aumentando 10 %, temos R${aumentar(p, 10)}')
+print(f'Diminuindo 5 %, temos R${diminuir(p, 5)}')
